aa837196a9a59f9b9fad64256506b380f7c98ddb
Tuan. Nguyen Anh - CMC Global DU1.22 <natuan17@cmcglobal.vn>
2022-12-09 17:25:56 +0700

    Build base service

---
+++ server/src/main/java/com/example/server/service/ServerService.java
@@ -0,0 +1,16 @@
+package com.example.server.service;
+
+import com.example.server.model.Server;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Collection;
+
+public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+    Server ping(String ipAddress) throws IOException;
+}
---
+++ server/src/main/java/com/example/server/service/impl/ServerServiceImpl.java
@@ -0,0 +1,83 @@
+package com.example.server.service.impl;
+
+import com.example.server.enumeration.Status;
+import com.example.server.model.Server;
+import com.example.server.repo.ServerRepository;
+import com.example.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Random;
+
+import static java.lang.Boolean.TRUE;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+
+    private final ServerRepository serverRepository;
+
+    @Override
+    public Server create(Server server) {
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Fetching all server");;
+        return serverRepository.findAll(PageRequest.of(0,limit)).toList();
+    }
+
+    @Override
+    public Server get(Long id) {
+        log.info("Fetching server by id: {}", id);
+        Optional<Server> byId = serverRepository.findById(id);
+        if(byId.isPresent()){
+            return byId.get();
+        }
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Update server: {}", server.getName());
+        return serverRepository.save(server);
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        log.info("Delete server by Id: {}", id);
+        serverRepository.deleteById(id);
+        return TRUE;
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Ping server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
+    }
+
+    private String setServerImageUrl() {
+        String[] imageNames = {"server1.png","server2.png","server3.png","server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(4)]).toUriString();
+    }
+
+}
